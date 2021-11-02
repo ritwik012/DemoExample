cd8bde56305373419a63ef314fa7349dddc52c44
“ritwik012” <rutwikgulhaneg@gmail.com>
2021-11-02 09:35:45 +0530

    [Rutwik] add : created a project with Demo

---
+++ WhileLoop/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine(" i = {0}", i);
+                i++;
+            }
+        }
+    }
+}
